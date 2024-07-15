85f7b58916c8fa69990ebead98012e8a14381552
Adnan <adnanmuhammedadnancp@gmail.com>
2024-07-15 11:53:42 +0530

    add new endpoint

--- main.py
+++ main.py
@@ -36,14 +36,10 @@ async def create_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     return responds
 
 
-@app.get("/api/users/id/{user_id}")
-async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    responds = crud.get_user_by_id(db, user_id)
-    return responds if responds else HTTPException(status_code=404)
-
-
 @app.get("/api/users/books/list")
-async def get_all_book(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def list_all_books(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
     response = crud.get_all_book(db, skip, limit)
     return response if response else HTTPException(status_code=404)
 
@@ -53,16 +49,28 @@ async def create_book(book: LibraryCreateSchema, db: Session = Depends(get_db)):
     return crud.create_book_by_user(db, book)
 
 
+@app.get("/api/users/id/{user_id}")
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    responds = crud.get_user_by_id(db, user_id)
+    return responds if responds else HTTPException(status_code=404)
+
+
+@app.get("/api/users/books/id/{book_id}")
+async def get_book_by_id(book_id: int, db: Session = Depends(get_db)):
+    responds = crud.get_book_by_id(db, book_id)
+    return responds if responds else HTTPException(status_code=404)
+
+
 @app.put("/api/users/books/update/{book_id}")
-async def update_book(
+async def update_book_by_id(
     book_id: int, updated_book: LibraryUpdateSchema, db: Session = Depends((get_db))
 ):
     responds = crud.update_book(db, book_id, updated_book)
     return responds if responds else HTTPException(status_code=404)
 
 
-@app.delete("/api/users/books/delete/{user_id}")
-async def delete_book(book_id: int, db: Session = Depends(get_db)):
+@app.delete("/api/users/books/delete/{book_id}")
+async def delete_book_by_id(book_id: int, db: Session = Depends(get_db)):
     responds = crud.delete_book(db, book_id)
     return responds if responds else HTTPException(status_code=202)
 
